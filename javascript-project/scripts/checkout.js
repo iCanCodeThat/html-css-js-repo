61405c93df639b6824c18442929c76d956dc4649
kes demata <kkfdemata03@gmail.com>
2023-09-14 21:45:00 +0800

    Generate HTML for the order summary

---
+++ javascript-project/scripts/checkout.js
@@ -0,0 +1,104 @@
+import { cart } from "../data/cart.js";
+import { products } from "../data/products.js";
+
+//variable to store result in combining the HTML
+let cartSummaryHTML = '';
+
+cart.forEach((cartItem)=>{
+  //  we create variable and use the productId to search product to get the other details such as the image.
+  //  get productId out of the cartItem.
+  const productId = cartItem.productId; 
+
+  let matchingProduct; //create variable to save the result
+  
+  //Then we Loop through products array from products.js
+  //  for each product, were gonna check if the ID property from products.js is equal to the productId from checkout.js
+  products.forEach((product)=>{
+    if(product.id === productId){
+      matchingProduct = product;
+    }
+  });
+
+  cartSummaryHTML += `
+      <div class="cart-item-container">
+      <div class="delivery-date">
+        Delivery date: Tuesday, June 21
+      </div>
+
+      <div class="cart-item-details-grid">
+        <img class="product-image"
+          src="${matchingProduct.image}">
+
+        <div class="cart-item-details">
+          <div class="product-name">
+            ${matchingProduct.name}
+          </div>
+          <div class="product-price">
+            ${matchingProduct.priceCents / 100}
+          </div>
+          <div class="product-quantity">
+            <span>
+              Quantity: <span class="quantity-label">${cart.quantity}</span>
+            </span>
+            <span class="update-quantity-link link-primary">
+              Update
+            </span>
+            <span class="delete-quantity-link link-primary">
+              Delete
+            </span>
+          </div>
+        </div>
+
+        <div class="delivery-options">
+          <div class="delivery-options-title">
+            Choose a delivery option:
+          </div>
+          <div class="delivery-option">
+            <input type="radio" checked
+              class="delivery-option-input"
+              name="delivery-option-1">
+            <div>
+              <div class="delivery-option-date">
+                Tuesday, June 21
+              </div>
+              <div class="delivery-option-price">
+                FREE Shipping
+              </div>
+            </div>
+          </div>
+          <div class="delivery-option">
+            <input type="radio"
+              class="delivery-option-input"
+              name="delivery-option-1">
+            <div>
+              <div class="delivery-option-date">
+                Wednesday, June 15
+              </div>
+              <div class="delivery-option-price">
+                $4.99 - Shipping
+              </div>
+            </div>
+          </div>
+          <div class="delivery-option">
+            <input type="radio"
+              class="delivery-option-input"
+              name="delivery-option-1">
+            <div>
+              <div class="delivery-option-date">
+                Monday, June 13
+              </div>
+              <div class="delivery-option-price">
+                $9.99 - Shipping
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+  `;
+});
+
+//to generate the HTML with js to the checkout.html
+document.querySelector('.js-order-summary')
+  .innerHTML = cartSummaryHTML;
