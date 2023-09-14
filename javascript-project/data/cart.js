7d59dd09c2ef11708d3102743ff066d6b283e341
kes demata <kkfdemata03@gmail.com>
2023-09-14 22:18:20 +0800

    Delete products from cart when clicking delete

--- javascript-project/data/cart.js
+++ javascript-project/data/cart.js
@@ -1,5 +1,5 @@
 //export the cart variable to make it accessible outside cart.js
-export const cart = [{
+export let cart = [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2,
 }, {
@@ -30,4 +30,18 @@ export function addToCart(productId){ //when we call this function
       quantity: 1
     });
   }
+}
+
+//function for removing product from a cart
+
+export function removeFromCart(productId){
+  const newCart = [];
+
+  cart.forEach((cartItem)=>{
+    if(cartItem.productId !== productId){
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
 }
--- javascript-project/scripts/checkout.js
+++ javascript-project/scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -44,7 +44,7 @@ cart.forEach((cartItem)=>{
             <span class="update-quantity-link link-primary">
               Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
               Delete
             </span>
           </div>
@@ -103,3 +103,13 @@ cart.forEach((cartItem)=>{
 //to generate the HTML with js to the checkout.html
 document.querySelector('.js-order-summary')
   .innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link')
+  .forEach((link) =>{
+    link.addEventListener('click', () => {
+      const productId = link.dataset.productId;
+
+      removeFromCart(productId);
+      console.log(cart);
+    });
+  });
